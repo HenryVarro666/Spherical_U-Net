d432a0b9b329fbdc1c688500d7c2155643437943
HenryVarro666 <1504517223@qq.com>
2024-07-14 17:19:13 -0400

    vtk 2 dataset with SDF

--- vtk_SDF_dataset.py
+++ vtk_SDF_dataset.py
@@ -2,127 +2,101 @@
 Author: HenryVarro666 1504517223@qq.com
 Date: 1969-12-31 19:00:00
 LastEditors: HenryVarro666 1504517223@qq.com
-LastEditTime: 2024-07-14 16:48:14
+LastEditTime: 2024-07-14 17:02:09
 FilePath: /Spherical_U-Net/vtk_2_dataloader.py
 '''
 # from sphericalunet.utils.vtk import read_vtk
 import pyvista
 import numpy as np
 from nibabel.freesurfer import io as fio
+from scipy.spatial import cKDTree
 
 import torch
 import glob
 import os
-
 import vtk
 
-
-# # Example 1
-# def delete_this():
-#     """
-#     This function reads geometry and morph data from files and saves it in a VTK file format.
-#     It performs the following steps:
-#     1. Reads points and faces from a geometry file.
-#     2. Reads sulc, curv, and thickness data from morph files.
-#     3. Creates a PolyData object using the points and faces.
-#     4. Assigns sulc, curv, and thickness data to the PolyData object.
-#     5. Saves the PolyData object in VTK file format.
-
-#     Returns:
-#     - None
-#     """
-#     points, faces = fio.read_geometry('/media/jialec/DATA/HCP_download_for_weiyan/100206_3T_Structural_preproc_extended/100206/T1w/100206/surf/lh.sphere')
-#     sulc = fio.read_morph_data('/media/jialec/DATA/HCP_download_for_weiyan/100206_3T_Structural_preproc_extended/100206/T1w/100206/surf/lh.sulc')
-#     curv = fio.read_morph_data('/media/jialec/DATA/HCP_download_for_weiyan/100206_3T_Structural_preproc_extended/100206/T1w/100206/surf/lh.curv')
-#     thickness = fio.read_morph_data('/media/jialec/DATA/HCP_download_for_weiyan/100206_3T_Structural_preproc_extended/100206/T1w/100206/surf/lh.thickness')
-
-#     # points, faces = fio.read_geometry('/usr/local/freesurfer/7.2.0/subjects/fsaverage/surf/lh.sphere')
-#     sulc = fio.read_morph_data('/usr/local/freesurfer/7.2.0/subjects/fsaverage/surf/lh.sulc')
-#     curv = fio.read_morph_data('/usr/local/freesurfer/7.2.0/subjects/fsaverage/surf/lh.curv')
-#     # par_fs = fio.read_annot('/usr/local/freesurfer/7.2.0/subjects/fsaverage/label/lh.aparc.a2005s.annot')
-#     # par_fs_2009 = fio.read_annot('/usr/local/freesurfer/7.2.0/subjects/fsaverage/label/lh.aparc.a2009s.annot')
-
-#     faces = np.concatenate((np.ones((faces.shape[0], 1))*3, faces), axis=-1).astype(np.int32)
-#     surf = pyvista.PolyData(points, faces)
-#     surf['sulc'] = sulc
-#     surf['curv'] = curv
-#     surf['thickness'] = thickness
-#     # surf['par_fs'] = par_fs
-#     # surf['par_fs_2009'] = par_fs_2009
-#     # surf.save('./delete_this.vtk', binary=False)
-#     surf.save('./delete_this_hcp.fsaverage.vtk', binary=False)
-#     print("True")
-#     return
-
-# # Example 2
-# def read_vtk(in_file):
-#     """
-#     Read .vtk POLYDATA file
+def read_vtk(in_file):
+    """
+    Read .vtk POLYDATA file
     
-#     in_file: string,  the filename
-#     Out: dictionary, 'vertices', 'faces', 'curv', 'sulc', ...
-#     """
-#     polydata = pyvista.read(in_file)
+    in_file: string,  the filename
+    Out: dictionary, 'vertices', 'faces', 'sulc', 'curv', ...
+    """
+    polydata = pyvista.read(in_file)
     
-#     # n_faces = polydata.n_faces
-
-#     n_faces = polydata.n_faces_strict
-
-#     # import pdb; pdb.set_trace()
-#     vertices = np.array(polydata.points)  # get vertices coordinate
-
-#     # only for triangles polygons data
-#     faces = np.array(polydata.GetPolys().GetData())  # get faces connectivity
-#     assert len(faces)/4 == n_faces, "faces number is wrong!"
-#     faces = np.reshape(faces, (n_faces,4))
+    n_faces = polydata.n_faces_strict
+    vertices = np.array(polydata.points)  # get vertices coordinate
+    faces = np.array(polydata.faces).reshape(-1, 4)[:, 1:]  # get faces connectivity
     
-#     data = {'vertices': vertices,
-#             'faces': faces
-#             }
+    data = {'vertices': vertices,
+            'faces': faces}
     
-#     point_data = polydata.point_data
-#     for key, value in point_data.items():
-#         if value.dtype == 'uint32':
-#             data[key] = np.array(value).astype(np.int64)
-#         elif  value.dtype == 'uint8':
-#             data[key] = np.array(value).astype(np.int32)
-#         else:
-#             data[key] = np.array(value)
+    point_data = polydata.point_data
+    for key, value in point_data.items():
+        data[key] = np.array(value)
+    
+    return data
 
-#     return data
+def write_vtk(in_dic, file, binary=True):
+    """
+    Write .vtk POLYDATA file
+    
+    in_dic: dictionary, vtk data
+    file: string, output file name
+    """
+    vertices = in_dic['vertices']
+    faces = in_dic['faces']
+    polydata = pv.PolyData(vertices, faces)
+    for key, value in in_dic.items():
+        if key not in ['vertices', 'faces']:
+            polydata.point_data[key] = value
+    polydata.save(file, binary=binary)
 
-def read_vtk(file_path):
-    reader = vtk.vtkPolyDataReader()
-    reader.SetFileName(file_path)
-    reader.Update()
-    return reader.GetOutput()
+def compute_sdf(surface_points, line_points):
+    tree = cKDTree(line_points)
+    distances, _ = tree.query(surface_points)
+    return distances
 
-def vtk_to_numpy(vtk_data):
-    points = vtk_data.GetPoints()
-    num_points = points.GetNumberOfPoints()
-    features = np.zeros((num_points, 3))
+def save_sdf_npy(feature_file, line_file, output_dir):
+    surface_data = read_vtk(feature_file)
+    line_data = read_vtk(line_file)
 
-    for i in range(num_points):
-        features[i, :] = points.GetPoint(i)
+    surface_points = surface_data['vertices']
+    line_points = line_data['vertices']
 
-    return features
+    sdf_values = compute_sdf(surface_points, line_points)
+
+    # 将 SDF 值添加为特征数据
+    surface_data['sdf'] = sdf_values
+
+    # 保存为 npy 文件
+    output_filename = os.path.join(output_dir, os.path.basename(feature_file).replace('.vtk', '_sdf.npy'))
+    np.save(output_filename, surface_data)
+
+# 示例用法
+feature_file = 'path/to/surface.vtk'
+line_file = 'path/to/line.vtk'
+output_dir = 'path/to/output_dir'
+
+save_sdf_npy(feature_file, line_file, output_dir)
 
 
 
-class BrainDatasetSDF(torch.utils.data.Dataset):
-    def __init__(self, data_dir, transform=None):
-        self.data_files = sorted(glob.glob(os.path.join(data_dir, '*_sdf.npy')))
-        self.transform = transform
+# class BrainDatasetSDF(torch.utils.data.Dataset):
+#     def __init__(self, data_dir, transform=None):
+#         self.data_files = sorted(glob.glob(os.path.join(data_dir, '*_sdf.npy')))
+#         self.transform = transform
 
-    def __len__(self):
-        return len(self.data_files)
+#     def __len__(self):
+#         return len(self.data_files)
 
-    def __getitem__(self, idx):
-        data_path = self.data_files[idx]
-        data = np.load(data_path)
-        features, sdf = data[:, :-1], data[:, -1]
+#     def __getitem__(self, idx):
+#         data_path = self.data_files[idx]
+#         data = np.load(data_path)
+#         features, sdf = data[:, :-1], data[:, -1]
 
-        if self.transform:
-            features, sdf = self.transform(features, sdf)
+#         if self.transform:
+#             features, sdf = self.transform(features, sdf)
 
-        return torch.tensor(features, dtype=torch.float32), torch.tensor(sdf, dtype=torch.float32)
+#         return torch.tensor(features, dtype=torch.float32), torch.tensor(sdf, dtype=torch.float32)
