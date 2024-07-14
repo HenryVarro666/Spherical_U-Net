057d2d3911ce09d776ed0f81d562c8c9a6c91d54
HenryVarro666 <1504517223@qq.com>
2024-07-14 17:53:35 -0400

    prepare dataset

--- vtk_SDF_dataset.py
+++ vtk_SDF_dataset.py
@@ -2,15 +2,14 @@
 Author: HenryVarro666 1504517223@qq.com
 Date: 1969-12-31 19:00:00
 LastEditors: HenryVarro666 1504517223@qq.com
-LastEditTime: 2024-07-14 17:02:09
+LastEditTime: 2024-07-14 17:52:53
 FilePath: /Spherical_U-Net/vtk_2_dataloader.py
 '''
 # from sphericalunet.utils.vtk import read_vtk
-import pyvista
+import pyvista as pv
 import numpy as np
 from nibabel.freesurfer import io as fio
 from scipy.spatial import cKDTree
-
 import torch
 import glob
 import os
@@ -23,35 +22,17 @@ def read_vtk(in_file):
     in_file: string,  the filename
     Out: dictionary, 'vertices', 'faces', 'sulc', 'curv', ...
     """
-    polydata = pyvista.read(in_file)
+    polydata = pv.read(in_file)
+    vertices = np.array(polydata.points)
+    faces = np.array(polydata.faces).reshape(-1, 4)[:, 1:]
     
-    n_faces = polydata.n_faces_strict
-    vertices = np.array(polydata.points)  # get vertices coordinate
-    faces = np.array(polydata.faces).reshape(-1, 4)[:, 1:]  # get faces connectivity
-    
-    data = {'vertices': vertices,
-            'faces': faces}
+    data = {'vertices': vertices, 'faces': faces}
     
     point_data = polydata.point_data
     for key, value in point_data.items():
         data[key] = np.array(value)
     
     return data
-
-def write_vtk(in_dic, file, binary=True):
-    """
-    Write .vtk POLYDATA file
-    
-    in_dic: dictionary, vtk data
-    file: string, output file name
-    """
-    vertices = in_dic['vertices']
-    faces = in_dic['faces']
-    polydata = pv.PolyData(vertices, faces)
-    for key, value in in_dic.items():
-        if key not in ['vertices', 'faces']:
-            polydata.point_data[key] = value
-    polydata.save(file, binary=binary)
 
 def compute_sdf(surface_points, line_points):
     tree = cKDTree(line_points)
@@ -66,37 +47,21 @@ def save_sdf_npy(feature_file, line_file, output_dir):
     line_points = line_data['vertices']
 
     sdf_values = compute_sdf(surface_points, line_points)
-
-    # 将 SDF 值添加为特征数据
     surface_data['sdf'] = sdf_values
 
-    # 保存为 npy 文件
     output_filename = os.path.join(output_dir, os.path.basename(feature_file).replace('.vtk', '_sdf.npy'))
     np.save(output_filename, surface_data)
 
 # 示例用法
-feature_file = 'path/to/surface.vtk'
-line_file = 'path/to/line.vtk'
-output_dir = 'path/to/output_dir'
+if __name__ == "__main__":
+    # 硬编码的路径
+    for subject in os.listdir("data"):
+        feature_file = 'path/to/your/surface.vtk'
+        line_file = 'path/to/your/line.vtk'
+        output_dir = './Dataset_npy'
 
-save_sdf_npy(feature_file, line_file, output_dir)
+    save_sdf_npy(feature_file, line_file, output_dir)
 
 
 
-# class BrainDatasetSDF(torch.utils.data.Dataset):
-#     def __init__(self, data_dir, transform=None):
-#         self.data_files = sorted(glob.glob(os.path.join(data_dir, '*_sdf.npy')))
-#         self.transform = transform
 
-#     def __len__(self):
-#         return len(self.data_files)
-
-#     def __getitem__(self, idx):
-#         data_path = self.data_files[idx]
-#         data = np.load(data_path)
-#         features, sdf = data[:, :-1], data[:, -1]
-
-#         if self.transform:
-#             features, sdf = self.transform(features, sdf)
-
-#         return torch.tensor(features, dtype=torch.float32), torch.tensor(sdf, dtype=torch.float32)
