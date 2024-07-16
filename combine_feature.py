83ef19362821ab143063a71695e7bc195991cf1e
Chao Cao <1504517223@qq.com>
2024-07-16 12:55:29 -0400

    work on longleaf

--- combine_feature.py
+++ combine_feature.py
@@ -1,8 +1,8 @@
 '''
 Author: HenryVarro666 1504517223@qq.com
 Date: 1969-12-31 19:00:00
-LastEditors: HenryVarro666 1504517223@qq.com
-LastEditTime: 2024-07-15 13:19:23
+LastEditors: Chao Cao 1504517223@qq.com
+LastEditTime: 2024-07-16 12:41:59
 FilePath: /Spherical_U-Net/combine_feature.py
 '''
 
@@ -20,16 +20,17 @@ def combine():
 
     surf['curv'] = curv_value
     surf['sulc'] = sulc_value
-    # surf.save(surf_path.replace('.32k_fs_LR.Sphere.vtk', '.withGrad.32k_fs_LR.Sphere.vtk'), binary=False)
-    surf.save('/mnt/d/Spherical_U-Net/examples/new.vtk', binary=False)
+    surf.save(surf_path.replace('.InnerSurf.RegByFS.Resp40962.vtk', '.InnerSurf.RegByFS.Resp40962.vtk'), binary=False)
+    print(f'{subject} {hemi} Done')
+    # surf.save('/mnt/d/Spherical_U-Net/examples/new.vtk', binary=False)
 
 if __name__ == '__main__':
-    root_dir = '/mnt/d/Spherical_U-Net/examples/'
+    root_dir = '/work/users/c/h/chaocao/For_Training'
     for subject in os.listdir(root_dir):
         if re.match(r'^\d+$', subject):
             for hemi in ['lh', 'rh']:
-                surf_path = os.path.join(root_dir, subject +'_recon_40962', f'{subject}.{hemi}.InnerSurf.RegByFS.Resp40962.vtk')
-                curv_path = os.path.join(root_dir, subject +'_gyralnet_island_40962',f'{hemi}.curv')
-                sulc_path = os.path.join(root_dir, subject +'_gyralnet_island_40962',f'{hemi}.sulc')
+                surf_path = os.path.join(root_dir, subject, subject +'_recon_40962', 'surf', f'{subject}.{hemi}.InnerSurf.RegByFS.Resp40962.vtk')
+                curv_path = os.path.join(root_dir, subject, subject +'_recon_40962','surf', f'{hemi}.curv')
+                sulc_path = os.path.join(root_dir, subject, subject +'_recon_40962','surf', f'{hemi}.sulc')
                 combine()
                 print('Done')
